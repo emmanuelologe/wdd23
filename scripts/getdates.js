febf363457c9574a1dea1364b0786fe64bcafb4a
emmanuelologe <epicraez@gmail.com>
2023-11-11 09:00:18 +0100

    Home paage Enhancement

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,36 @@
-const currentYear = new Date().getFullYear();
+// const currentYear = new Date().getFullYear();
 
-const copyrightLine = document.getElementById('copyright');
-copyrightLine.innerHTML = `© ${currentYear} 😎Emmanuel Ologe😎, Nigeria 🇳🇬`;
+// const copyrightLine = document.getElementById('copyright');
+// copyrightLine.innerHTML = `© ${currentYear} 😎Emmanuel Ologe😎, Nigeria 🇳🇬`;
 
 
-const lastModifiedParagraph = document.getElementById('lastModified');
-const lastModified = new Date(document.lastModified);
-lastModifiedParagraph.textContent = `Last modified: ${lastModified.toLocaleString()}`;
+// const lastModifiedParagraph = document.getElementById('lastModified');
+// const lastModified = new Date(document.lastModified);
+// lastModifiedParagraph.textContent = `Last modified: ${lastModified.toLocaleString()}`;
+
+// const menuToggle = document.getElementById('menu');
+// const nav = document.getElementById('nav');
+
+// menuToggle.addEventListener('click', () => {
+//   nav.classList.toggle('show');
+// });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const currentYear = new Date().getFullYear();
+    const copyrightLine = document.getElementById('copyright');
+    copyrightLine.innerHTML = `© ${currentYear} 😎Emmanuel Ologe😎, Nigeria 🇳🇬`;
+  
+    const lastModifiedParagraph = document.getElementById('lastModified');
+    const lastModified = new Date(document.lastModified);
+    lastModifiedParagraph.textContent = `Last modified: ${lastModified.toLocaleString()}`;
+  
+    const menuToggle = document.getElementById('menu');
+    const nav = document.querySelector('nav');
+  
+    menuToggle.addEventListener('click', () => {
+      nav.classList.toggle('show');
+    });
+  });
+  
